ead2bd76cdca5729b393fbbb1ce592d5991b3ba7
Fer-nomas <ferdanielavilacuevas@gmail.com>
2023-01-13 16:00:59 -0300

    links en la parte de normativas

--- cliente/src/routes/SectionGuides.svelte
+++ cliente/src/routes/SectionGuides.svelte
@@ -1,4 +1,4 @@
-<div
+<!-- <div
 	class="hover:bg-slate-200 cursor-pointer rounded border-l-4 border-orange-500 bg-slate-100 m-2 ml-5 py-3 text-lg"
 >
 	<p class="ml-3 text-slate-500">Catastro</p>
@@ -7,4 +7,13 @@
 	class="hover:bg-slate-200 cursor-pointer rounded border-l-4 border-red-500 bg-slate-100 m-2 ml-5 py-3 text-lg"
 >
 	<p class="ml-3 text-slate-500">Manuales e Instructivos</p>
-</div>
+</div> -->
+
+
+<a href="https://www.catastro.gov.py/public/6bc3d8_ley%201909-02%20loteamientos.pdf" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Ley de Loteamiento</a>
+<a href="https://www.catastro.gov.py/archivos/decretos/Relacionadas%20al%20Catastro/Decreto%2051-52.pdf" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Ley de Impuesto Inmobiliario</a>
+<a href="https://www.catastro.gov.py/archivos/costos%20y%20requisitos/requisitos/Certificados%20Catastrales.pdf" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Requsitos para tener un Inmueble </a>
+<a href="https://www.asuncion.gov.py/wp-content/uploads/2016/04/ORDEN-2012-217-REGLAMENTA-construccion-y-uso-de-VEREDAS-deroga-6-9250-9425-96-y-329-06VIGENTE.pdf" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Reglas para la Creacion de Veredas </a>
+<a href="https://www.asuncion.gov.py/wp-content/uploads/2022/07/ORD-1990-26104-REGLAMENTO-GENERAL-DE-LA-CONSTRUCCION.pdf" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Reglamento General de Construccion </a>
+<a href="https://www.bacn.gov.py/leyes-paraguayas/177/ley-n-1257-reglamenta-la-venta-de-tierras-loteadas" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Ley sobre Ventas de Lotes </a>
+<a href="https://www.bacn.gov.py/leyes-paraguayas/9332/ley-n-6380-de-modernizacion-y-simplificacion-del-sistema-tributario-nacional" target="_blank" rel="noreferrer" class="text-blue-600 hover:text-blue-800 ml-7 mt-2">Ley sobre Sistema Tributario </a>
